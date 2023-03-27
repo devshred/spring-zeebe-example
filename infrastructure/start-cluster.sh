42b32febb3f8a95e788092bfddad6525618b1c04
Johannes Schmidt <johannes.schmidt@1und1.de>
2023-03-27 19:26:52 +0200

    use latest 8.2 version of operate
    Systems running on Apple M1 chips are supported since 8.2

--- infrastructure/start-cluster.sh
+++ infrastructure/start-cluster.sh
@@ -2,7 +2,6 @@
 
 ### load helm charts provided by Camunda and NGINX
 helm repo add camunda https://helm.camunda.io
-helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 
 ### create Kubernetes cluster
@@ -15,10 +14,11 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 helm install \
   --set operate.ingress.enabled=true \
   --set operate.ingress.host=operate.local \
+  --set operate.image.tag=8.2.0-alpha5 \
   --set tasklist.ingress.enabled=true \
   --set tasklist.ingress.host=tasklist.local \
   -f https://raw.githubusercontent.com/camunda/camunda-platform-helm/main/kind/camunda-platform-core-kind-values.yaml \
   camunda \
   camunda/camunda-platform
 
-kubectl port-forward svc/camunda-zeebe-gateway 26500:26500 -n default
+kubectl port-forward svc/camunda-zeebe-gateway 26500:26500
